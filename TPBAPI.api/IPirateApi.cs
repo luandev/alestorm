f180666de620a41a0d0dea4e91f16e68df17ebac
Luan <luan@fake.com>
2018-08-20 21:42:07 +0100

    Fix namespace

---
+++ TPBAPI.api/IPirateApi.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TPBAPI.api
+{
+    public interface IPirateApi
+    {
+        Task<IEnumerable<PirateItem>> Search(string query, PirateCategory category);
+    }
+}
