b34af2151fb355e08780918ffedae43f8f6021be
Luan <luan@fake.com>
2018-08-20 21:42:35 +0100

    fix namespace

--- TPBAPI.webui/Startup.cs
+++ TPBAPI.webui/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
+using TPBAPI.api;
 
 namespace TPBAPI.webui
 {
