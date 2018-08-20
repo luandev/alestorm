57c35bf37df54790036fdf012e5c0bbc0fbb8a75
Luan <luan@fake.com>
2018-08-20 22:03:28 +0100

    Mongo conn

--- TPBAPI.webui/Startup.cs
+++ TPBAPI.webui/Startup.cs
@@ -28,7 +28,7 @@ namespace TPBAPI.webui
         public void ConfigureServices(IServiceCollection services)
         {
             /* Hangfire */
-            services.AddHangfire(x => x.UseMongoStorage("mongodb://127.0.0.1:27017/?readPreference=primary", "Hangfire"));
+            services.AddHangfire(x => x.UseMongoStorage("mongodb://127.0.0.1:27017", "Hangfire"));
 
             services.Configure<CookiePolicyOptions>(options =>
             {
