cd0d7c71b6742443b881e8d0e1532c0754091ff9
Luan <luan@fake.com>
2018-08-26 10:04:48 +0100

    forgot this little config

--- TPBAPI.web/Startup.cs
+++ TPBAPI.web/Startup.cs
@@ -76,8 +76,8 @@ namespace TPBAPI.web
 
                 if (env.IsDevelopment())
                 {
-                    //spa.UseReactDevelopmentServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
                 }
             });
         }
